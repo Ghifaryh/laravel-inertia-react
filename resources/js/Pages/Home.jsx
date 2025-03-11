187150f852ebeff349207277ce98da559db68792
Ghifaryh <mhghifaryy@gmail.com>
2025-03-11 11:33:01 +0700

    done tutorial number 7

--- resources/js/Pages/Home.jsx
+++ resources/js/Pages/Home.jsx
@@ -1,7 +1,11 @@
+import { Link } from "@inertiajs/react";
+
 export default function Home() {
     return (
         <>
-            <h1 className="title">Hello Users</h1>
+            <h1 className="title">Hello User</h1>
+
+            <Link preserveScroll href="" className="block title mt-[1000px]"> { new Date().toLocaleTimeString() } </Link>
         </>
     )
 }
--- routes/web.php
+++ routes/web.php
@@ -1,12 +1,19 @@
 <?php
 
+use App\Http\Controllers\PostController;
 use Illuminate\Support\Facades\Route;
 use Inertia\Inertia;
 
-Route::get('/', function () {
-    return Inertia::render('Home');
-});
+// Route::get('/', function () {
+//     sleep(1);
+//     return Inertia::render('Home');
+// });
 
-Route::get('/about', function () {
-    return inertia('About/About'); // didalam folder about
-});
+
+// Route::get('/about', function () {
+//     return inertia('About/About'); // didalam folder about
+// });
+
+Route::get('/', [PostController::class, 'index']);
+
+Route::resource('posts', PostController::class)->except('index');
